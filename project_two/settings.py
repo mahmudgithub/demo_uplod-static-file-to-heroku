ebad6893cd6cfa98e4088839f45967c755794f3f
mahmud555 <you@example.com>
2019-09-09 16:48:05 +0600

    name

--- project_two/settings.py
+++ project_two/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'project_two.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':'django.db.backends.postgresql',
+        'NAME':'project4',
+        'user':'postgres',
+        'Password':'mahmud2580',
+        'HOST':'172.0.0.1',
+        'PORT':'5432',
     }
 }
 
